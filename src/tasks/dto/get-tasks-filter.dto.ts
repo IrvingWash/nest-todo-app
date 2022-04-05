b36d297868c027a2e090a9aa9f3ae12620bfde06
Washington Irving <iamwashingyou@gmail.com>
2022-04-05 23:47:12 +0300

    validate get task with filters endpoint

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+	@IsOptional()
+	@IsEnum(TaskStatus)
 	public status?: TaskStatus;
+
+	@IsOptional()
+	@IsString()
 	public search?: string;
 }
