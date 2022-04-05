74bca7015132b18555aefd8e449e878f55466708
Washington Irving <iamwashingyou@gmail.com>
2022-04-05 03:58:27 +0300

    create tasks service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from "@nestjs/common";
 
+import { TasksService } from "./tasks.service";
+
 @Controller()
-export class TasksController {}
+export class TasksController {
+	constructor(private tasksService: TasksService) {}
+}
