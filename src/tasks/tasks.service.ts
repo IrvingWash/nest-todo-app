077627df98b85909dcf8067cf84fd7c7dc434c00
Washington Irving <iamwashingyou@gmail.com>
2022-04-05 05:21:49 +0300

    add get tasks with filter endpoint

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { v4 as uuid } from 'uuid';
+
 import { CreateTaskDto } from "./dto/create-task-dto";
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 
 import { Task, TaskStatus } from "./task.model";
 
@@ -10,6 +12,31 @@ export class TasksService {
 
 	public getAllTasks(): Task[] {
 		return this.tasks;
+	}
+
+	public getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+		const { status, search } = filterDto;
+
+		let tasks = this.getAllTasks();
+
+		if (status) {
+			tasks = tasks.filter((task) => task.status === status);
+		}
+
+		if (search) {
+			tasks = tasks.filter((task) => {
+				if (
+					task.title.toLowerCase().includes(search) ||
+					task.description.toLowerCase().includes(search)
+				) {
+					return true;
+				}
+
+				return false;
+			});
+		}
+
+		return tasks;
 	}
 
 	public getTaskById(id: string): Task {
