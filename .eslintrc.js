816bc583a3b31e06943e55d066afa5e1621341ce
Washington Irving <iamwashingyou@gmail.com>
2022-04-05 03:34:15 +0300

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint/eslint-plugin'],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	ignorePatterns: ['.eslintrc.js'],
+	rules: {
+		'eol-last': ['error', 'always'],
+		'semi': ['error', 'always']
+	},
+};
