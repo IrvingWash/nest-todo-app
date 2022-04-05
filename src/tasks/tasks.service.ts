3fbb5835d7b756da7ad4b58c98c3fad6f4630c0c
Washington Irving <iamwashingyou@gmail.com>
2022-04-05 05:03:21 +0300

    create patch task status endpoint

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -31,6 +31,14 @@ export class TasksService {
 		return task;
 	}
 
+	public updateTaskStatus(id: string, status: TaskStatus): Task {
+		const task = this.getTaskById(id);
+
+		task.status = status;
+
+		return task;
+	}
+
 	public deleteTask(id: string): void {
 		this.tasks = this.tasks.filter((task) => task.id !== id);
 	}
